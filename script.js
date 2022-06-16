275da7565585240e950eeafe9f606be41e5b22a6
hossein_abz76 <habaszadeh76@gmail.com>
2022-06-17 03:07:12 +0430

    home added

--- script.js
+++ script.js
@@ -3,26 +3,52 @@ let $ = document;
 let searchBtn = $.querySelector("#search-btn");
 let cartBtn = $.querySelector("#cart-btn");
 let loginBtn = $.querySelector("#login-btn");
+let menuBtn = $.querySelector("#menu-btn");
 let searchForm = $.querySelector(".search-form-container");
 let cartForm = $.querySelector(".shopping-cart-container");
 let loginForm = $.querySelector(".login-form-container");
-let navbar = document.querySelector(".header .navbar");
+let navbar = $.querySelector(".header .navbar");
 
 searchBtn.onclick = () => {
   searchForm.classList.toggle("active");
   cartForm.classList.remove("active");
   loginForm.classList.remove("active");
-    navbar.classList.remove("active");
+  navbar.classList.remove("active");
 };
 cartBtn.onclick = () => {
   searchForm.classList.remove("active");
   cartForm.classList.toggle("active");
   loginForm.classList.remove("active");
-    navbar.classList.remove("active");
+  navbar.classList.remove("active");
 };
 loginBtn.onclick = () => {
   searchForm.classList.remove("active");
   cartForm.classList.remove("active");
   loginForm.classList.toggle("active");
-    navbar.classList.remove("active");
+  navbar.classList.remove("active");
 };
+menuBtn.onclick = () => {
+  searchForm.classList.remove("active");
+  cartForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+$.querySelector('.home').onmousemove = (e) =>{
+
+    let x = (window.innerWidth - e.pageX * 2) / 90;
+    let y = (window.innerHeight - e.pageY * 2) / 90;
+
+    $.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px) translateY(${x}px)`;
+    $.querySelector('.home .home-parallax-img2').style.transform = `translateX(${y}px) translateY(${x}px)`;
+}
+
+$.querySelector('.home').onmouseleave = () =>{
+
+    $.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
+    $.querySelector('.home .home-parallax-img2').style.transform = `translateX(0px) translateY(0px)`;
+}  
