1e9d64c98a8b5d45ea2f044385088cf1ece62b42
hossein_abz76 <habaszadeh76@gmail.com>
2022-06-17 02:00:20 +0430

    search form added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let $ = document
+let $ = document;
+
+let searchForm = $.querySelector(".search-form-container");
+let searchBtn = $.querySelector("#search-btn");
+let cartForm = $.querySelector(".shopping-cart-container");
+let loginForm = $.querySelector(".login-form-container");
+let navbar = document.querySelector(".header .navbar");
+
+searchBtn.onclick = () => {
+  searchForm.classList.toggle("active");
+//   cartForm.classList.remove("active");
+//   loginForm.classList.remove("active");
+//   navbar.classList.remove("active");
+  console.log('click');
+};
+
